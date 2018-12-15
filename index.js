6cda57c8ec619d7ae22b99d9bfc886b83b1e5227
authanand <anandbabu.web@gmail.com>
2018-12-16 02:32:08 +0800

    indented js code

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ byline = require('byline');
 
 var inputArray = [];
 
-function bootsrap(){
-  var file;
-  var robo = new ToyRobo();
-if(process.argv.length == 2) file=process.argv[2];
-else file = fs.createReadStream(process.argv[2]);
+function bootsrap() {
+    var file;
+    var robo = new ToyRobo();
+    if (process.argv.length == 2) file = process.argv[2];
+    else file = fs.createReadStream(process.argv[2]);
 
-file.on('error', function(error) { console.log("Can't open file.");});
-var stream = byline(fs.createReadStream(process.argv[2], { encoding: 'utf8' }));
+    file.on('error', function(error) {
+        console.log("Can't open file.");
+    });
+    var stream = byline(fs.createReadStream(process.argv[2], {
+        encoding: 'utf8'
+    }));
 
-  stream.on('data', function(line) {
-    inputArray.push(line);
-  });
+    stream.on('data', function(line) {
+        inputArray.push(line);
+    });
 
-  stream.on('end', function(line) {
-    robo.loadData(inputArray);
-  });
-  
+    stream.on('end', function(line) {
+        robo.loadData(inputArray);
+    });
+
 }
 
 bootsrap();
-
-
-
-
